e520ccc30502fcfa3ca766cc0a14ec59ce3f5919
Hadas Nahon <Hadas@Hadas-Laptop.(none)>
2014-04-07 22:39:42 -0400

    add upload button to any comment element in the page (fix #1)

--- github-attach.js
+++ github-attach.js
@@ -1,9 +1,24 @@
-var button = document.createElement("input");
-button.type = "button";
-button.value = "Upload File...";
-button.setAttribute("class", "button");
-button.addEventListener("click", function() {
-    alert("Clicked");
-});
-var div = document.getElementById("js-new-comment-form-actions");
-div.insertBefore(button, div.firstChild);
+var button;
+var comments = document.getElementsByClassName('comment');
+for (var commentIndex = 0; commentIndex < comments.length; commentIndex++) {
+    addUploadButton(comments[commentIndex], 'minibutton');
+}
+var newComment = document.getElementsByClassName('timeline-new-comment')[0];
+addUploadButton(newComment, 'button');
+
+function addUploadButton(commentDiv, buttonClass) {
+    var buttonContainer;
+    var formActions = commentDiv.getElementsByClassName('form-actions')[0];
+    if (!formActions) {
+        return;
+    }
+    button = document.createElement("input");
+    button.type = "button";
+    button.value = "Upload File...";
+    button.setAttribute("class", buttonClass);
+    button.addEventListener("click", function() {
+        alert("Clicked");
+    });
+    buttonContainer = formActions.getElementsByTagName('button')[0].parentNode;
+    buttonContainer.insertBefore(button, buttonContainer.firstChild);
+}
