daa38e1cdae1a90ba89b4fdd159289608ee56115
Hadas Nahon <Hadas@Hadas-Laptop.(none)>
2014-04-07 23:27:37 -0400

    Open file dialog when clicking the upload file button

--- github-attach.js
+++ github-attach.js
@@ -1,4 +1,3 @@
-var button;
 var comments = document.getElementsByClassName('comment');
 for (var commentIndex = 0; commentIndex < comments.length; commentIndex++) {
     addUploadButton(comments[commentIndex], 'minibutton');
@@ -7,18 +6,40 @@ var newComment = document.getElementsByClassName('timeline-new-comment')[0];
 addUploadButton(newComment, 'button');
 
 function addUploadButton(commentDiv, buttonClass) {
+    var uploadInput;
+    var button;
     var buttonContainer;
+    var commentTextArea;
     var formActions = commentDiv.getElementsByClassName('form-actions')[0];
     if (!formActions) {
         return;
     }
+    buttonContainer = formActions.getElementsByTagName('button')[0].parentNode;
+    
+    commentTextArea = commentDiv.getElementsByClassName('comment-form-textarea')[0];
+    uploadInput = document.createElement("input");
+    uploadInput.setAttribute("style", "display:none");
+    uploadInput.type = "file";
+    uploadInput.addEventListener('change', function() {
+        var comment = 'Implement file upload (' + uploadInput.value + ')';
+        commentTextArea.value = commentTextArea.value ? commentTextArea.value + '\n' + comment : comment;
+    });
+    buttonContainer.insertBefore(uploadInput, buttonContainer.firstChild);
+    
+    button = createUploadButton(buttonClass);
+    button.addEventListener("click", function() {
+        //TODO: use JQuery
+        var clickEvent = document.createEvent('MouseEvents');
+        clickEvent.initEvent('click', true, true);
+        uploadInput.dispatchEvent(clickEvent);
+    });
+    buttonContainer.insertBefore(button, buttonContainer.firstChild);
+}
+
+function createUploadButton(buttonClass) {
     button = document.createElement("input");
     button.type = "button";
     button.value = "Upload File...";
     button.setAttribute("class", buttonClass);
-    button.addEventListener("click", function() {
-        alert("Clicked");
-    });
-    buttonContainer = formActions.getElementsByTagName('button')[0].parentNode;
-    buttonContainer.insertBefore(button, buttonContainer.firstChild);
+    return button;
 }
